e5c09e96c12a9544cd8d5b8b49c42d2f845a6ee2
Samandar-14188 <157939075+Samandar-14188@users.noreply.github.com>
2024-08-01 09:34:51 +0500

    error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { Link, useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
+  const error = useRouteError();
+
   return (
-    <div>ErrorPage</div>
-  )
+    <div className="error-container container">
+      {error.status === 404 ? (
+        <div>
+          <h3>Come back with a coffee - sometimes the pages find themselves! ☕</h3>
+          <Link to="/" className="btn">Go To Home</Link>
+        </div>
+      ) : (
+        <div>
+          <h3>Oops! Looks like our web page did a somersault amd landed in a digital rabbit hole. We're working on untangling the code. Hang tight!☕</h3>
+          <Link to="/" className="btn">Go To Home</Link>
+        </div>      )}
+    </div>
+  );
 }
